3cd2698c51db9357b48087a5339063aa90ec0f1a
Adam Yuldashev <adamyuldashev1997@gmail.com>
2023-05-21 09:40:44 -0700

    ok

--- src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
+++ src/main/java/lang/print/gaps/finalModuleTask/DigitsSumCalculator.java
@@ -2,5 +2,20 @@ package lang.print.gaps.finalModuleTask;
 
 public class DigitsSumCalculator {
     public void calculateSum(int number) {
+
+        int num2 = number % 10;
+
+        int num1 = number / 10;
+        int num3 = num1 % 10;
+
+        int num4 = num1 / 10;
+        int num5 = num4 % 10;
+
+        int num6 = num4 / 10;
+
+        System.out.println(num2 + num3 + num5 + num6);
+
     }
+
 }
+
